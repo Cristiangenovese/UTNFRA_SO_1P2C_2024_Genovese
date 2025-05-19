c4971f89e47594ef4cdc3d66809b2563648d1856
Cristian Genovese <cristiangenovese96@gmail.com>
2025-05-19 01:50:02 +0000

    ADD: Prueba script particion de disco

---
+++ RTA_SCRIPT_Examen_20250518/Punto_B.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PARTICION=$(lsblk -o NAME,SIZE -d | grep "10G" | grep -v sda | awk ¡P '{print "/dev/"$1}')
+
+for i in {1..10}; do
+echo -e "n\np\n$i\n\n+1G\nw" | sudo fdisk $PARTICION
+done
+
+echo "Tabla de particiones"
+sudo fdisk -l $PARTiCION
+
